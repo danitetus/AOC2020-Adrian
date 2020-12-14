3283d59820ff0e940d6ffce622e5e5eb15de94e0
kore <koregfx@gmail.com>
2020-12-14 19:21:21 +0100

    day4 problem 2 not working

--- src/day04/day04_problem02.ts
+++ src/day04/day04_problem02.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 
-const empyPasport: Ipasport = {
+const emptyPassport: Ipasport = {
     byr: '',
     iyr: '',
     eyr: '',
@@ -15,11 +15,12 @@ const empyPasport: Ipasport = {
 
 function PasportArrange(array: string[]): Ipasport[] {
     let dataArranged: Ipasport[] = [];
-    let pasport: Ipasport = empyPasport;
+    let pasport: Ipasport = { ...emptyPassport };
     for (let line of array) {
         if (line === '') {
-            dataArranged.push(pasport);
-            pasport = empyPasport;
+            const pasportDetach = { ...pasport };
+            dataArranged.push(pasportDetach);
+            pasport = { ...emptyPassport };
         } else {
             const lineSplit = line.split(':');
             switch (lineSplit[0]) {
@@ -56,7 +57,57 @@ function PasportArrange(array: string[]): Ipasport[] {
 function countValidPasport(array: Ipasport[]): number {
     let result = 0;
 
+    for (let passport of array) {
+        let validationCount = 0;
+        const Byear = parseInt(passport.byr);
+        if (Byear >= 1920 && Byear <= 2002) {
+            validationCount++;
+        }
+        const Iyear = parseInt(passport.iyr);
+        if (Iyear >= 2010 && Iyear <= 2020) {
+            validationCount++;
+        }
+        const Eyear = parseInt(passport.eyr);
+        if (Eyear >= 2020 && Eyear <= 2030) {
+            validationCount++;
+        }
 
+        const heightValue = parseInt(passport.hgt)
+        if (heightValue >= 150 && heightValue <= 192) {
+            if (passport.hgt.slice(3) === 'cm') {
+                validationCount++;
+            }
+        }
+        if (heightValue >= 59 && heightValue <= 76) {
+            if (passport.hgt.slice(2) === 'in') {
+                validationCount++;
+            }
+        }
+        let matchColor = /^#[\da-f]{6}$/;
+        let matchHairColor = matchColor.exec(passport.hcl);
+        if (matchHairColor) {
+            if (matchHairColor[0] == matchHairColor.input) {
+                validationCount++;
+            }
+        }
+        let eyematch = /^(amb|blu|brn|gry|grn|hzl|oth)$/;
+        let matchEyeColor = eyematch.exec(passport.ecl);
+        if (matchEyeColor) {
+            if (matchEyeColor[0] == matchEyeColor.input) {
+                validationCount++;
+            }
+        }
+        let matchId = /[0-9]{9}/;
+        let matchPassportId = matchId.exec(passport.pid);
+        if (matchPassportId) {
+            if (matchPassportId[0] == matchPassportId.input) {
+                validationCount++;
+            }
+        }
+        if (validationCount == 7) {
+            result++;
+        }
+    }
     return result;
 }
 const start = async (): Promise<number> => {
@@ -65,7 +116,7 @@ const start = async (): Promise<number> => {
     let data = inputData.toString().split('\n').map(x => {
         return x.split(' ').map(y => y.replace('\r', ''));
     }).reduce((a, b) => a.concat(b));
-    PasportArrange(data);
+    console.log(countValidPasport(PasportArrange(data)));
     return 0;
 }
 
